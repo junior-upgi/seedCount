223fa14e488d6923941c7f7e313817d32d129297
junior-upgi <junior@upgi.com.tw>
2016-11-02 13:28:26 +0800

    start writing queries in the database
    added seedCountResult to calculate the result for every single entry of data

---
+++ backend/sql_script/001_seedCountResult.sql
@@ -0,0 +1,111 @@
+SELECT
+	DATEADD(day,-1,CAST(d.recordDatetime AS DATE)) AS recordDate
+	,CAST(d.recordDatetime AS TIME) AS recordTime
+	,d.recordDatetime
+	,d.prodFacilityID
+	,d.prodLineID
+	,d.prodReference
+	,d.count_0
+	,d.count_1
+	,d.count_2
+	,d.count_3
+	,d.count_4
+	,d.count_5
+	,d.validEntryCount
+	,d.avgCount
+	,d.thickness
+	,d.unitSeedCount
+	,d.note
+	,d.photoLocation
+	,d.created
+FROM (
+	SELECT b.recordDatetime
+		  ,b.prodFacilityID
+		  ,b.prodLineID
+		  ,b.prodReference
+		  ,b.count_0
+		  ,b.count_1
+		  ,b.count_2
+		  ,b.count_3
+		  ,b.count_4
+		  ,b.count_5
+		  ,c.validEntryCount
+		  ,ROUND(CAST(ISNULL(b.count_0,0)+ISNULL(b.count_1,0)+ISNULL(b.count_2,0)+ISNULL(b.count_3,0)+ISNULL(b.count_4,0)+ISNULL(b.count_5,0) AS FLOAT)/c.validEntryCount,2) AS avgCount
+		  ,b.thickness
+		  ,ROUND((1*b.thickness/10)*CAST(ISNULL(b.count_0,0)+ISNULL(b.count_1,0)+ISNULL(b.count_2,0)+ISNULL(b.count_3,0)+ISNULL(b.count_4,0)+ISNULL(b.count_5,0) AS FLOAT)/c.validEntryCount,2) AS unitSeedCount
+		  ,b.note
+		  ,b.photoLocation
+		  ,b.created
+	FROM productionHistory.dbo.seedCount AS b
+		LEFT JOIN (
+			SELECT a.recordDatetime, a.prodFacilityID, a.prodLineID, COUNT(*) AS validEntryCount
+			FROM (
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_0 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_1 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_2 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_3 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_4 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_5 IS NOT NULL) AS a
+			GROUP BY a.recordDatetime, a.prodFacilityID, a.prodLineID) AS c ON (b.recordDatetime=c.recordDatetime AND b.prodFacilityID=c.prodFacilityID AND b.prodLineID=c.prodLineID)) AS d
+WHERE CAST(d.recordDatetime AS TIME) BETWEEN '00:00' AND '06:00'
+UNION ALL
+SELECT
+	CAST(d.recordDatetime AS DATE) AS recordDate
+	,CAST(d.recordDatetime AS TIME) AS recordTime
+	,d.recordDatetime
+	,d.prodFacilityID
+	,d.prodLineID
+	,d.prodReference
+	,d.count_0
+	,d.count_1
+	,d.count_2
+	,d.count_3
+	,d.count_4
+	,d.count_5
+	,d.validEntryCount
+	,d.avgCount
+	,d.thickness
+	,d.unitSeedCount
+	,d.note
+	,d.photoLocation
+	,d.created
+FROM (
+	SELECT b.recordDatetime
+		  ,b.prodFacilityID
+		  ,b.prodLineID
+		  ,b.prodReference
+		  ,b.count_0
+		  ,b.count_1
+		  ,b.count_2
+		  ,b.count_3
+		  ,b.count_4
+		  ,b.count_5
+		  ,c.validEntryCount
+		  ,ROUND(CAST(ISNULL(b.count_0,0)+ISNULL(b.count_1,0)+ISNULL(b.count_2,0)+ISNULL(b.count_3,0)+ISNULL(b.count_4,0)+ISNULL(b.count_5,0) AS FLOAT)/c.validEntryCount,2) AS avgCount
+		  ,b.thickness
+		  ,ROUND((1*b.thickness/10)*CAST(ISNULL(b.count_0,0)+ISNULL(b.count_1,0)+ISNULL(b.count_2,0)+ISNULL(b.count_3,0)+ISNULL(b.count_4,0)+ISNULL(b.count_5,0) AS FLOAT)/c.validEntryCount,2) AS unitSeedCount
+		  ,b.note
+		  ,b.photoLocation
+		  ,b.created
+	FROM productionHistory.dbo.seedCount AS b
+		LEFT JOIN (
+			SELECT a.recordDatetime, a.prodFacilityID, a.prodLineID, COUNT(*) AS validEntryCount
+			FROM (
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_0 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_1 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_2 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_3 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_4 IS NOT NULL
+				UNION ALL
+				SELECT * FROM productionHistory.dbo.seedCount WHERE count_5 IS NOT NULL) AS a
+			GROUP BY a.recordDatetime, a.prodFacilityID, a.prodLineID) AS c ON (b.recordDatetime=c.recordDatetime AND b.prodFacilityID=c.prodFacilityID AND b.prodLineID=c.prodLineID)) AS d
+WHERE CAST(d.recordDatetime AS TIME) NOT BETWEEN '00:00' AND '06:00';
