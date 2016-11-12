c4af75638402861d1e6dfe2a8d4d7354b4ad204c
junior-upgi <junior@upgi.com.tw>
2016-11-13 01:14:30 +0800

    added telegram information

---
+++ backend/telegram.js
@@ -0,0 +1,34 @@
+var botList = [{
+        id: "267738010",
+        first_name: "逾期款機器人",
+        username: "overdueMonitorBot",
+        token: "267738010:AAGT17aLumIfVPNeFWht8eUEPuC2HfAouGk",
+        joinedChatGroupID: [-150874076]
+    },
+    {
+        id: "251686312",
+        first_name: "氣泡數機器人",
+        username: "seedCountBot",
+        token: "251686312:AAG8_sczOJvJSwtese4kgzH95RLyX5ZJ114",
+        joinedChatGroupID: [-155069392]
+    }
+];
+
+var userList = [{
+    id: 241630569,
+    user_name: "junior_upgi",
+    first_name: "佳佑",
+    last_name: "蔡",
+    status: ["admin", "user"],
+    joinedChatGroupID: [-150874076, -155069392]
+}];
+
+var chatGroupList = [{
+    id: -150874076,
+    title: "業務"
+}, {
+    id: -155069392,
+    title: "玻璃製造"
+}];
+
+var botAPIurl = "https://api.telegram.org/bot";
