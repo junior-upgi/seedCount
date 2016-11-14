fc6628351eecfb51514a878a51d20a4d9a6dcffe
junior-upgi <junior@upgi.com.tw>
2016-11-14 18:59:00 +0800

    added a sequence property to prodLineList model data object

--- frontend/js/model.js
+++ frontend/js/model.js
@@ -101,34 +101,42 @@ var facilityList = [{
 
 var prodLineList = [{
     reference: 'L1-1',
+    sequence: 0,
     side: 'west',
     hidden: true
 }, {
     reference: 'L1',
+    sequence: 1,
     side: 'west',
     hidden: false
 }, {
     reference: 'L2',
+    sequence: 2,
     side: 'west',
     hidden: false
 }, {
     reference: 'L3',
+    sequence: 3,
     side: 'center',
     hidden: true
 }, {
     reference: 'L5',
+    sequence: 4,
     side: 'center',
     hidden: true
 }, {
     reference: 'L6',
+    sequence: 5,
     side: 'center',
     hidden: false
 }, {
     reference: 'L7',
+    sequence: 6,
     side: 'east',
     hidden: false
 }, {
     reference: 'L8',
+    sequence: 7,
     side: 'east',
     hidden: false
 }];
