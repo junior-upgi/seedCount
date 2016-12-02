80769afc5f4e0387762485d283ed1948c9258947
junior-upgi <junior@upgi.com.tw>
2016-12-02 08:15:46 +0800

    change server access url

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var serverHost = "http://localhost"; //development
 var serverPort = process.env.PORT || 9002;
 var broadcastServerHost = "http://upgi.ddns.net";
 var broadcastServerPort = process.env.PORT || 9001;
-var mssqlServerHost = "http://upgi.ddns.net"; // access database from the internet (development)
-//var mssqlServerHost = "http://192.168.168.5"; // access database from LAN (production)
+//var mssqlServerHost = "http://upgi.ddns.net"; // access database from the internet (development)
+var mssqlServerHost = "http://192.168.168.5"; // access database from LAN (production)
 var upgiSystemAccount = "upgiSystem";
 var upgiSystemPassword = "upgiSystem";
 
